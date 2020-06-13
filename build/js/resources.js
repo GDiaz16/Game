28dbbb8a260783b0da8492c03b7f34c979d48421
GDiaz16 <egonzalodm@gmail.com>
2020-06-13 04:43:48 -0500

    fix gitignore

---
+++ build/js/resources.js
@@ -0,0 +1,92 @@
+game.resources = [
+    {
+        "name": "dst-inertexponent",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "cling",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "stomp",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "dungeon-sheet",
+        "type": "image",
+        "src": "data/img/map/dungeon-sheet.png"
+    },
+    {
+        "name": "Sample",
+        "type": "image",
+        "src": "data/img/map/kenney_prototypePack_2.3/Sample.png"
+    },
+    {
+        "name": "prototype-sheet",
+        "type": "image",
+        "src": "data/img/map/prototype-sheet.png"
+    },
+    {
+        "name": "box",
+        "type": "image",
+        "src": "data/img/sprite/box.png"
+    },
+    {
+        "name": "man",
+        "type": "image",
+        "src": "data/img/sprite/man.png"
+    },
+    {
+        "name": "Project05C-1024x896",
+        "type": "image",
+        "src": "data/img/sprite/Project05C-1024x896.png"
+    },
+    {
+        "name": "purple-man",
+        "type": "image",
+        "src": "data/img/sprite/purple-man.png"
+    },
+    {
+        "name": "spinning_coin_gold",
+        "type": "image",
+        "src": "data/img/sprite/spinning_coin_gold.png"
+    },
+    {
+        "name": "escenario",
+        "type": "tmx",
+        "src": "data/map/escenario.tmx"
+    },
+    {
+        "name": "dungeon-min",
+        "type": "tsx",
+        "src": "data/map/dungeon-min.tsx"
+    },
+    {
+        "name": "dungeon",
+        "type": "tsx",
+        "src": "data/map/dungeon.tsx"
+    },
+    {
+        "name": "prototype",
+        "type": "tsx",
+        "src": "data/map/prototype.tsx"
+    },
+    {
+        "name": "prueba",
+        "type": "tsx",
+        "src": "data/map/prueba.tsx"
+    }
+];
